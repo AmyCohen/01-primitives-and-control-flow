c783de803684813c81398377c5b39a6af2763090
Amy Cohen <anbcohen@gmail.com>
2018-07-10 11:53:49 -0700

    corrected indention

--- CommandLineClock/src/CommandLineClock.java
+++ CommandLineClock/src/CommandLineClock.java
@@ -4,19 +4,19 @@ import java.util.concurrent.TimeUnit;
 public class CommandLineClock {
     public static void main(String[] args) throws InterruptedException {
 
-            while (true) {
-                LocalDateTime now = LocalDateTime.now();
-                String hourString = Integer.toString(now.getHour());
-                String minuteString = Integer.toString(now.getMinute());
-                String secondString = Integer.toString(now.getSecond());
+        while (true) {
+            LocalDateTime now = LocalDateTime.now();
+            String hourString = Integer.toString(now.getHour());
+            String minuteString = Integer.toString(now.getMinute());
+            String secondString = Integer.toString(now.getSecond());
 
             String hour = paddedTime(hourString);
-                String minute = (paddedTime(minuteString));
-                String second = (paddedTime(secondString));
+            String minute = (paddedTime(minuteString));
+            String second = (paddedTime(secondString));
 
-                System.out.println(hour + ":" + minute + ":" + second);
-                TimeUnit.SECONDS.sleep(1);
-            }
+            System.out.println(hour + ":" + minute + ":" + second);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
     public static String paddedTime(String time) {
